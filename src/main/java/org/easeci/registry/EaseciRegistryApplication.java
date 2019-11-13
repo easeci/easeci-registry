a6caa47481b3aaa22de13cfd3eb27ec45d1f2fc4
meksula <meksula.karol.93@gmail.com>
2019-11-13 21:26:19 +0100

    Java 13 project generated

---
+++ src/main/java/org/easeci/registry/EaseciRegistryApplication.java
@@ -0,0 +1,13 @@
+package org.easeci.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EaseciRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EaseciRegistryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/easeci/registry/EaseciRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.easeci.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EaseciRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
